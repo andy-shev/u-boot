ea6d8bd58377c063be01555dca83fbb6ef513c29
Razvan Becheriu <razvan@isc.org>
2019-09-30 09:22:16 +0300

    adding part command support for block size and partition number

--- cmd/part.c
+++ cmd/part.c
@@ -24,6 +24,8 @@
 enum cmd_part_info {
 	CMD_PART_INFO_START = 0,
 	CMD_PART_INFO_SIZE,
+	CMD_PART_INFO_BLOCK,
+	CMD_PART_INFO_NUMBER,
 };
 
 static int do_part_uuid(int argc, char * const argv[])
@@ -121,6 +123,7 @@ static int do_part_info(int argc, char * const argv[], enum cmd_part_info param)
 	int part;
 	int err;
 	int ret;
+	int id = 0;
 
 	if (argc < 3)
 		return CMD_RET_USAGE;
@@ -132,7 +135,7 @@ static int do_part_info(int argc, char * const argv[], enum cmd_part_info param)
 		return 1;
 
 	part = simple_strtoul(argv[2], &endp, 0);
-	if (*endp == '\0') {
+	if (*endp == '\0' && param != CMD_PART_INFO_NUMBER) {
 		err = part_get_info(desc, part, &info);
 		if (err)
 			return 1;
@@ -142,12 +145,33 @@ static int do_part_info(int argc, char * const argv[], enum cmd_part_info param)
 			return 1;
 	}
 
+	if (param == CMD_PART_INFO_NUMBER) {
+		int p;
+		disk_partition_t info;
+
+		for (p = 1; p < MAX_SEARCH_PARTITIONS; p++) {
+			int r = part_get_info(desc, p, &info);
+			if (r != 0)
+				continue;
+			if (!strcmp(info.name, argv[2])) {
+				id = p;
+				break;
+			}
+		}
+	}
+
 	switch (param) {
 	case CMD_PART_INFO_START:
 		snprintf(buf, sizeof(buf), LBAF, info.start);
 		break;
 	case CMD_PART_INFO_SIZE:
 		snprintf(buf, sizeof(buf), LBAF, info.size);
+		break;
+	case CMD_PART_INFO_BLOCK:
+		snprintf(buf, sizeof(buf), LBAF, info.blksz);
+		break;
+	case CMD_PART_INFO_NUMBER:
+		snprintf(buf, sizeof(buf), LBAF, id);
 		break;
 	default:
 		printf("** Unknown cmd_part_info value: %d\n", param);
@@ -172,6 +196,16 @@ static int do_part_size(int argc, char * const argv[])
 	return do_part_info(argc, argv, CMD_PART_INFO_SIZE);
 }
 
+static int do_part_block(int argc, char * const argv[])
+{
+	return do_part_info(argc, argv, CMD_PART_INFO_BLOCK);
+}
+
+static int do_part_number(int argc, char * const argv[])
+{
+	return do_part_info(argc, argv, CMD_PART_INFO_NUMBER);
+}
+
 static int do_part(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
 	if (argc < 2)
@@ -185,6 +219,10 @@ static int do_part(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 		return do_part_start(argc - 2, argv + 2);
 	else if (!strcmp(argv[1], "size"))
 		return do_part_size(argc - 2, argv + 2);
+	else if (!strcmp(argv[1], "block"))
+		return do_part_block(argc - 2, argv + 2);
+	else if (!strcmp(argv[1], "number"))
+		return do_part_number(argc - 2, argv + 2);
 
 	return CMD_RET_USAGE;
 }
@@ -206,5 +244,11 @@ U_BOOT_CMD(
 	"      part can be either partition number or partition name\n"
 	"part size <interface> <dev> <part> <varname>\n"
 	"    - set environment variable to the size of the partition (in blocks)\n"
-	"      part can be either partition number or partition name"
+	"      part can be either partition number or partition name\n"
+	"part block <interface> <dev> <part> <varname>\n"
+	"    - set environment variable to the size of the partition block\n"
+	"      part can be either partition number or partition name\n"
+	"part number <interface> <dev> <part> <varname>\n"
+	"    - set environment variable to the partition number\n"
+	"      part must be partition name"
 );
